ec6d02d3b52c5c438775d52ff442d2c9a9c6f4a0
Tiago <tiago.americano.03@gmail.com>
2021-02-18 16:32:24 -0300

    Corrigi bug no subscribe

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,10 +1,15 @@
 import {connectToDatabase} from '../../config/mongodb'
 
 export default async (req, res)=>{
-  const db = await connectToDatabase(process.env.MONGODB_URI)
+  const {db, client} = await connectToDatabase(process.env.MONGODB_URI)
+  if(client.isConnected()){
+
   const collection = db.collection('subscribers')
 
   await collection.insertOne({...req.body, subscriberAt: new Date()})
 
   return res.status('200').json({eai:true})
+  }
+  return res.status('500').json({ error: 'client DB is not connected' })
+
 }
